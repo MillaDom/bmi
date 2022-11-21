4466fa94f9489a673751bc7ea5e81f27c8d3290c
MillaDom <milladom@gmail.com>
2022-11-21 11:25:06 +0100

    1st check

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public double calculate(double m, double h) {
-        double index;
-            index = m / (h * h);
-        return index;
+    public double calculate(double weight, double height) {
+        double weightIndex;
+        weightIndex = weight / (height * height);
+        return weightIndex;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double m = 90;
-        double h = 1.70;
-        double bmi = service.calculate(m, h);
-        System.out.println(bmi);
+        double weight = 65;
+        double height = 1.69;
+        double bmi = service.calculate(weight, height);
+        System.out.println((int) bmi);
 
 
     }
